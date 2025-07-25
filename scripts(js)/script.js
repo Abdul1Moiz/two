aafb4e294fcd9ee7705add95136d8c5a807396ee
Abdul1Moiz <profello55@gmail.com>
2025-07-25 17:10:07 +0500

    deploy

--- scripts(js)/script.js
+++ scripts(js)/script.js
@@ -1,4 +1,15 @@
 
+function loadVideo(el) {
+    el.innerHTML = `
+          <iframe src="https://player.vimeo.com/video/1101743481?autoplay=1&muted=1&loop=1"
+                  frameborder="0" allow="autoplay; fullscreen"
+                  style="position:absolute;top:0;left:0;width:100%;height:100%;"
+                  loading="lazy"></iframe>`;
+}
+
+
+
+
 // laptop images slider JAVASCRIPT code
 document.addEventListener('DOMContentLoaded', function () {
     const slides = document.querySelectorAll('.screen-overlay .slide');
@@ -62,7 +73,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 var acc = document.getElementsByClassName("according");
 for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
+    acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
         this.parentElement.classList.toggle("active");
 
@@ -70,3 +81,39 @@ for (let i = 0; i < acc.length; i++) {
         panel.classList.toggle("active");
     });
 }
+
+
+
+
+
+
+// Deployment ID
+// AKfycbzbvFol_Sf1Q2193s0dReVZHwW9oIHB37sXJB7ZlqORLnV-s4GXamP-s9nGeOloAhzEew
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzbvFol_Sf1Q2193s0dReVZHwW9oIHB37sXJB7ZlqORLnV-s4GXamP-s9nGeOloAhzEew/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    msg.textContent = "Sending...";
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.textContent = "Message sent successfully";
+            setTimeout(() => {
+                msg.textContent = "";
+            }, 3000);
+            form.reset();
+        })
+        .catch(error => {
+            msg.textContent = "Something went wrong!";
+            setTimeout(() => {
+                msg.textContent = "";
+            }, 3000);
+            form.reset();
+        });
+});
